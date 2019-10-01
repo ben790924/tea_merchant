5c357d839a93c9ce7c843ca6ac37195bbc465ed6
BEN <37503754+ben790924@users.noreply.github.com>
2019-10-02 00:57:42 +0800

    center container

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,21 +1,22 @@
 <template>
-    <b-container>
-        <b-row>
-            <b-col lg="6" align-h="center" offset-lg="3">
-                <form>
-                    <p class="h4 text-center mb-4">登入</p>
-                    <div class="grey-text">
-                        <mdb-input label="你的信箱" icon="envelope" type="email" v-model="userLogin.email"/>
-                        <mdb-input label="你的密碼" icon="lock" type="password" v-model="userLogin.password" />
-                    </div>
-                    <div class="text-center">
-                        <div class="btn btn-success" @click="login">確認</div>
-                        <div class="btn btn-primary float-right">註冊</div>
-                    </div>
-                </form>
-            </b-col>
-        </b-row>
-    </b-container>
+    <div class="wrapper">
+        <b-container class="container">
+            <b-row>
+                <b-col lg="6" align-h="center" offset-lg="3">
+                    <form>
+                        <p class="h4 text-center mb-4">登入</p>
+                        <div class="grey-text">
+                            <mdb-input label="你的信箱" icon="envelope" type="email" v-model="userLogin.email"/>
+                            <mdb-input label="你的密碼" icon="lock" type="password" v-model="userLogin.password" />
+                        </div>
+                        <div class="text-center">
+                            <div class="btn btn-success" @click="login">確認</div>
+                        </div>
+                    </form>
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 <script>
 import { mdbInput } from 'mdbvue';
@@ -44,5 +45,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-
+.wrapper{
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    align-items: center;
+}
 </style>
