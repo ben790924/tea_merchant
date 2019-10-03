2a43e42cba502ac84d543c53bd4f8e394ebcd503
BEN <37503754+ben790924@users.noreply.github.com>
2019-10-03 18:45:36 +0800

    navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="/home">TeaShop</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">商品</b-nav-item>
+          </b-navbar-nav>
+
+          <b-navbar-nav class="ml-auto"> 
+            <b-navbar-nav right>
+              <b-nav-item @click="$router.push('/login')">登入</b-nav-item>
+              <b-nav-item @click="$router.push('/register')">註冊</b-nav-item>
+            </b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
