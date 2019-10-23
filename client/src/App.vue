42dedae79e9253d4dac26d9011453b33c52c1900
BEN <37503754+ben790924@users.noreply.github.com>
2019-10-23 18:14:28 +0800

    footer 完成

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,6 +28,41 @@
       <router-link to="/about">About</router-link>
     </div> -->
     <router-view/>
+    <b-container fluid class="footer">
+        <b-row>
+          <b-col>
+            <div class="about-us">
+              <div class="about-us-title">關於我們</div>
+              <div class="about-us-address">桃園市龍岡區後龍街35號</div>
+              <div class="about-us-tel">+886 981344218</div>
+              <div class="about-us-email">
+                <a href="mailto:ben790924@gmail.com" target="_blank">Email</a>
+              </div>
+            </div>
+          </b-col>
+          <b-col>
+            <div class="direct-buy">
+              <div class="direct-buy-title text-center">直接購買</div>
+              <b-form-input class="direct-buy-whatbuy" type="text" placeholder="要買什麼?"></b-form-input>
+              <b-form-input class="direct-buy-email" type="email" placeholder="輸入Email"></b-form-input>
+            </div>
+          </b-col>
+          <b-col>
+            <div class="dev-author">
+              <div class="dev-author-title">關於開發者</div>
+              <div class="dev-author-intro">
+                <a href="https://www.cakeresume.com/5afa59">Author</a>
+              </div>
+              <div class="dev-author-pork">
+                <div class="want-pork text-center">幫忙開發</div>
+                <a href="#"><i class="fas fa-code-branch"></i> PORK</a>
+                <a href=""><i class="fab fa-github"></i> Github</a>
+              </div>
+
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
   </div>
 </template>
 <script>
@@ -42,5 +77,68 @@ export default {
 #app{
   min-height: 100vh;
   font-family: '微軟正黑體';
+}
+.footer{
+  margin-top: 30px;
+  position: relative;
+  width: 100%;
+  bottom: 0;
+  height: 200px;
+  background-color: #FAFAFA;
+  color: #222;
+  .about-us{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px 0;
+    letter-spacing: 1px;
+    .about-us-title{
+      font-weight: bold;
+      font-size: 20px;
+    }
+    .about-us-email{
+      &>a{
+        color: #222;
+      }
+    }
+    &>div{
+      margin-bottom: 20px;
+      
+    }
+  }
+  .direct-buy{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 7px 0;
+    letter-spacing: 1px;
+    .direct-buy-title{
+      font-weight: bold;
+      font-size: 20px;
+      margin-bottom: 20px;
+    }
+    &>input{
+        margin-bottom: 20px;
+        width: 280px;
+      }
+  }
+  .dev-author{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 7px 0;
+    letter-spacing: 1px;
+    .dev-author-title{
+      font-size: 20px;
+      font-weight: bold;
+      margin-bottom: 20px;
+    }
+    &>div{
+      margin-bottom: 20px;
+    }
+  }
 }
 </style>
